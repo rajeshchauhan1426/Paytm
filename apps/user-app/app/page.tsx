498dbec1f9907c6d35cbf11cdf9a7ecc1d04a3af
rajeshchauhan1426 <codedemon1426@gmail.com>
2024-09-17 16:48:59 +0530

    prisma client

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import {PrismaClient} from "@repo/db/client"
 
+
+const client = new PrismaClient()
 export default function Home() {
   return (
     <div className="text-2xl">
